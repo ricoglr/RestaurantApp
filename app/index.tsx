be65d5647ddb7a4056ce2717bb43762d77b2c2da
ricoglr <rico.dev.tr@gmail.com>
2024-09-23 12:08:43 +0300

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,29 @@
+// In App.js in a new project
+
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SearchScreen from './screens/SearchScreen';
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer independent= {true}>
+      <Stack.Navigator screenOptions={{headerTitle:'Restaurant App'}}>
+        <Stack.Screen name="Search" component={SearchScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
